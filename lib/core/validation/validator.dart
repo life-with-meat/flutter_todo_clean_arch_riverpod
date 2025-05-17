c157ffe30279b85dbc4ee8703dcc6a0d80c0579e
life-with-meat <life.with.meat@gmail.com>
2025-05-17 18:28:00 +0900

    余分なコードを削除

--- lib/core/validation/validator.dart
+++ lib/core/validation/validator.dart
@@ -22,27 +22,4 @@ class Validator<T> {
     }
     return null;
   }
-
-  /// 指定された型のルールが存在するかチェック
-  bool hasRule<R extends ValidationRule>() {
-    return getRule<R>() != null;
-  }
-
-  /// すべてのルールを取得
-  List<ValidationRule> get allRules => List.unmodifiable(rules);
-
-  /// バリデーションルールを追加した新しいValidatorを作成
-  Validator<T> addRule(ValidationRule rule) {
-    return Validator([...rules, rule]);
-  }
-
-  /// 複数のバリデーションルールを追加した新しいValidatorを作成
-  Validator<T> addRules(List<ValidationRule> newRules) {
-    return Validator([...rules, ...newRules]);
-  }
-
-  /// 指定された型のルールを削除した新しいValidatorを作成
-  Validator<T> removeRule<R extends ValidationRule>() {
-    return Validator(rules.where((rule) => rule is! R).toList());
-  }
 }
